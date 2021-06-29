5c99f99364f0dca57bf348b44734dd770917776f
Matthias Zobrist <matthias.zobrist@zepi.net>
2021-06-29 07:34:10 +0200

    Fixed a bug with empty rating factor for rulesets and optimized the calculation

--- src/Rule/Tester/AbstractRuleTester.php
+++ src/Rule/Tester/AbstractRuleTester.php
@@ -10,19 +10,19 @@ abstract class AbstractRuleTester implements RuleTesterInterface
     protected function calculateSpamRating(RuleEntityInterface $rule, $item, $additionalFactor = 1): float
     {
         $rating = 1;
-        if (isset($item['rating']) && $item['rating'] != '') {
-            $rating = $item['rating'];
+        if (isset($item['rating']) && !empty($item['rating'])) {
+            $rating = floatval($item['rating']);
         }
 
         $ruleSpamRatingFactor = 1;
-        if ($rule->getSpamRatingFactor() != '') {
-            $ruleSpamRatingFactor = $rule->getSpamRatingFactor();
+        if (!empty($rule->getSpamRatingFactor())) {
+            $ruleSpamRatingFactor = floatval($rule->getSpamRatingFactor());
         }
 
         $rating = ($rating * $additionalFactor) * $ruleSpamRatingFactor;
 
-        if ($rule instanceof RulesetRuleCache) {
-            $rating = $rating * $rule->getRulesetCache()->getRuleset()->getSpamRatingFactor();
+        if ($rule instanceof RulesetRuleCache && !empty($rule->getRulesetCache()->getRuleset()->getSpamRatingFactor())) {
+            $rating = $rating * floatval($rule->getRulesetCache()->getRuleset()->getSpamRatingFactor());
         }
 
         return $rating;
