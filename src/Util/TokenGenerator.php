73807245c02e2b3b2b3d449029935db41afeaeeb
Matthias Zobrist <matthias.zobrist@zepi.net>
2022-06-06 14:33:05 +0200

    Added the copyright for the TokenGenerator

--- src/Util/TokenGenerator.php
+++ src/Util/TokenGenerator.php
@@ -6,6 +6,8 @@ namespace Mosparo\Util;
  * Class TokenGenerator
  * Source: https://github.com/FriendsOfSymfony/FOSUserBundle/blob/b6246f838e6ef2aa9b67ae4a5690a7978a209eff/Util/TokenGenerator.php
  *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
  * @package Mosparo\Util
  */
 class TokenGenerator
