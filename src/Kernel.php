c5287fcb06d475e00ca912ddd5285f4385481568
Matthias Zobrist <matthias.zobrist@zepi.net>
2022-12-24 15:17:10 +0100

    Bumped version number to 0.2.17

--- src/Kernel.php
+++ src/Kernel.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
-    const VERSION = '0.2.16';
+    const VERSION = '0.2.17';
 
     use MicroKernelTrait;
 }
