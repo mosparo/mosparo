2e083ec3fd8ef74e511a07fe2e0f65b77cfab3db
Matthias Zobrist <matthias.zobrist@zepi.net>
2023-03-09 17:51:52 +0100

    Bump version number to 0.3.7

--- src/Kernel.php
+++ src/Kernel.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
-    const VERSION = '0.3.6';
+    const VERSION = '0.3.7';
 
     use MicroKernelTrait;
 }
