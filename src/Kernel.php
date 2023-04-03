75bd63449f42ba1f613404bd851eb40fc2ea1693
Matthias Zobrist <matthias.zobrist@zepi.net>
2023-04-03 07:48:08 +0200

    Bumped the version number to v0.3.10

--- src/Kernel.php
+++ src/Kernel.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
-    const VERSION = '0.3.9';
+    const VERSION = '0.3.10';
 
     use MicroKernelTrait;
 }
