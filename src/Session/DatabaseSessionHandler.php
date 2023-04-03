b62918717e7631dca983737c4a76ac580eda7826
Matthias Zobrist <matthias.zobrist@zepi.net>
2023-04-03 07:46:30 +0200

    Fixed the missing sessions table after updating to v0.3.9

--- src/Session/DatabaseSessionHandler.php
+++ src/Session/DatabaseSessionHandler.php
@@ -15,8 +15,28 @@ use Symfony\Component\HttpFoundation\Session\Storage\Handler\PdoSessionHandler;
  */
 class DatabaseSessionHandler extends PdoSessionHandler
 {
-    public function __construct(Connection $connection, $options = [])
+    protected $mosparoVersion;
+
+    public function __construct(Connection $connection, $mosparoVersion, $options = [])
     {
+        $this->mosparoVersion = $mosparoVersion;
         parent::__construct($connection->getNativeConnection(), $options);
+    }
+
+    protected function doRead(string $sessionId)
+    {
+        /**
+         * This special check is needed because this database session handler was introduced in v0.3.9.
+         * Before, the table `sessions` does not exist. When the user tries to update mosparo in the browser,
+         * the session is required, so Symfony tries to initialize the session before creating the database,
+         * which ends in an error 500.
+         *
+         * With this fix, we ensure that the handler does not try to read from the table if the update is pending.
+         */
+        if (version_compare($this->mosparoVersion, '0.3.9', 'lt')) {
+            return '';
+        }
+
+        return parent::doRead($sessionId);
     }
 }
