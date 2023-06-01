cb0929ac49638820163b3cd8a6ea07d2d0a6a768
Matthias Zobrist <matthias.zobrist@zepi.net>
2023-06-01 19:42:02 +0200

    Added the upgrade logic to the update command

--- src/Command/UpdateMosparoCommand.php
+++ src/Command/UpdateMosparoCommand.php
@@ -2,6 +2,7 @@
 
 namespace Mosparo\Command;
 
+use Mosparo\Helper\SetupHelper;
 use Mosparo\Helper\UpdateHelper;
 use Mosparo\Kernel;
 use Mosparo\Message\UpdateMessage;
@@ -16,17 +17,20 @@ class UpdateMosparoCommand extends Command
 
     protected UpdateHelper $updateHelper;
 
+    protected SetupHelper $setupHelper;
+
     protected string $projectDirectory;
 
     protected $lastSection;
 
     protected ?UpdateMessage $lastMessage = null;
 
-    public function __construct(UpdateHelper $updateHelper, string $projectDirectory)
+    public function __construct(UpdateHelper $updateHelper, SetupHelper $setupHelper, string $projectDirectory)
     {
         parent::__construct();
 
         $this->updateHelper = $updateHelper;
+        $this->setupHelper = $setupHelper;
         $this->projectDirectory = $projectDirectory;
     }
 
@@ -74,21 +78,51 @@ class UpdateMosparoCommand extends Command
         }
 
         $this->updateHelper->checkForUpdates();
-        if (!$this->updateHelper->isUpdateAvailable()) {
+        if (!$this->updateHelper->isUpdateAvailable() && !$this->updateHelper->isUpgradeAvailable()) {
             $output->writeln('No updates available.');
 
             return Command::SUCCESS;
         }
 
-        $versionData = $this->updateHelper->getAvailableUpdateData();
+        if ($this->updateHelper->isUpdateAvailable() && $this->updateHelper->isUpgradeAvailable()) {
+            $output->writeln('');
+            $output->writeln('<fg=yellow>#</>');
+            $output->writeln('<fg=yellow># A new major version is available.</>');
+            $output->writeln('<fg=yellow># Please update to the latest version of your major version befor you can upgrade.</>');
+            $output->writeln('<fg=yellow>#</>');
+            $output->writeln('');
+        }
 
-        $output->writeln('<question> Update for mosparo available! </>');
+        $result = Command::SUCCESS;
+        if ($this->updateHelper->isUpdateAvailable()) {
+            // Proceed with the update
+
+            $output->writeln('<question> Update for mosparo available! </>');
+
+            $versionData = $this->updateHelper->getAvailableUpdateData();
+            $result = $this->updateMosparo($input, $output, $versionData);
+        } else if ($this->updateHelper->isUpgradeAvailable()) {
+            // Proceed with the upgrade
+
+            $output->writeln('<question> Upgrade for mosparo available! </>');
+
+            $upgradeData = $this->updateHelper->getAvailableUpgradeData();
+            $result = $this->upgradeMosparo($input, $output, $upgradeData);
+        }
+
+        return $result;
+    }
+
+    protected function updateMosparo(InputInterface $input, OutputInterface $output, array $versionData, $isUpgrade = false)
+    {
+        $helper = $this->getHelper('question');
+
         $output->writeln('');
         $output->writeln(sprintf("<fg=red>Installed version:</>\t<comment>%s</>", Kernel::VERSION));
         $output->writeln(sprintf("<fg=green>New version:</>\t\t<comment>%s</>", $versionData['number']));
         $output->writeln('');
 
-        $question = new ConfirmationQuestion(sprintf('Do you want to update mosparo to version %s? (n) ', $versionData['number']), false);
+        $question = new ConfirmationQuestion(sprintf('Do you want to %s mosparo to version %s? (n) ', ($isUpgrade) ? 'upgrade' : 'update', $versionData['number']), false);
 
         // Abort if the user didn't want to continue
         if (!$helper->ask($input, $output, $question)) {
@@ -98,11 +132,11 @@ class UpdateMosparoCommand extends Command
         $output->writeln('');
         $output->writeln('<fg=red>This command does not backup your mosparo installation and your database.</>');
         $output->writeln('');
-        $question = new ConfirmationQuestion('Do you want to continue with the update? (n) ', false);
+        $question = new ConfirmationQuestion(sprintf('Do you want to continue with the %s? (n) ', ($isUpgrade) ? 'upgrade' : 'update'), false);
 
         // Abort if the user didn't want to continue
         if (!$helper->ask($input, $output, $question)) {
-            $output->writeln('Update stopped');
+            $output->writeln(sprintf('%s stopped', ($isUpgrade) ? 'Upgrade' : 'Update'));
 
             return Command::SUCCESS;
         }
@@ -147,17 +181,113 @@ class UpdateMosparoCommand extends Command
         $this->updateHelper->updateMosparo($versionData);
 
         // Cleanup the content
-        $this->updateHelper->output(new UpdateMessage('global', UpdateMessage::STATUS_COMPLETED, 'Update completed'));
+        $this->updateHelper->output(new UpdateMessage('global', UpdateMessage::STATUS_COMPLETED, sprintf('%s completed', ($isUpgrade) ? 'Upgrade' : 'Update')));
 
         $output->writeln('');
-        $output->writeln('<bg=green;fg=black> Update finalized! </>');
+        $output->writeln(sprintf('<bg=green;fg=black> %s finished! </>', ($isUpgrade) ? 'Upgrade' : 'Update'));
         $output->writeln('');
-        $output->writeln('Please execute the migrations to finalize the update:');
+        $output->writeln(sprintf('Please execute the migrations to finalize the %s:', ($isUpgrade) ? 'upgrade' : 'update'));
         $output->writeln('');
         $output->writeln('  php bin/console doctrine:migrations:migrate');
         $output->writeln('  php bin/console cache:clear');
         $output->writeln('');
 
         return Command::SUCCESS;
+    }
+
+    protected function upgradeMosparo(InputInterface $input, OutputInterface $output, array $upgradeData)
+    {
+        $helper = $this->getHelper('question');
+
+        $majorVersionData = $upgradeData['majorVersionData'];
+        $versionData = $upgradeData['versionData'];
+
+        $output->writeln('');
+        $output->writeln(sprintf("<fg=red>Installed major version:</>\t<comment>%s</>", Kernel::MAJOR_VERSION));
+        $output->writeln(sprintf("<fg=green>New major version:</>\t\t<comment>%s</>", $majorVersionData['number']));
+        $output->writeln('');
+
+        $question = new ConfirmationQuestion(sprintf('Do you want to upgrade mosparo to the major version %s? (n) ', $majorVersionData['number']), false);
+
+        // Abort if the user didn't want to continue
+        if (!$helper->ask($input, $output, $question)) {
+            return Command::SUCCESS;
+        }
+
+        // Check the requirements
+        [ $meetPrerequisites, $prerequisites ] = $this->setupHelper->checkUpgradePrerequisites($majorVersionData ?? []);
+
+        if (!$meetPrerequisites) {
+            $output->writeln('');
+            $output->writeln('<fg=red>❌ Your web hosting does not meet all requirements.</>');
+            $output->writeln('');
+
+            foreach ($prerequisites as $type => $subPrerequisites) {
+                $name = $this->getTypeName($type);
+                $output->writeln($name);
+                $output->writeln(str_repeat('-', strlen($name)));
+
+                foreach ($subPrerequisites as $subType => $data) {
+                    $name = $this->getTypeName($type, $subType);
+                    $color = 'green';
+                    $icon = '✅ ';
+                    if ($data['required'] && !$data['pass']) {
+                        $color = 'red';
+                        $icon = '❌ ';
+                    }
+
+                    $output->writeln(sprintf('<fg=%s>%s%s</>', $color, $icon, $name));
+
+                    if ($type === 'general' && $subType === 'minPhpVersion' && $data['required'] && !$data['pass']) {
+                        $output->writeln('');
+                        $output->writeln(sprintf("<fg=red>Available:</>\t<fg=red>  %s</>", $data['available']));
+                        $output->writeln(sprintf("<fg=green>Required:</>\t<fg=green>≥ %s</>", $data['required']));
+                    }
+                }
+
+                $output->writeln('');
+            }
+
+            return Command::FAILURE;
+        }
+
+        $output->writeln('');
+        $output->writeln('<fg=green>✅ Your web hosting meets all requirements for this new major version.</>');
+        $output->writeln('');
+        $question = new ConfirmationQuestion('Do you want to continue with the upgrade? (n) ', false);
+
+        // Abort if the user didn't want to continue
+        if (!$helper->ask($input, $output, $question)) {
+            $output->writeln('Upgrade aborted');
+
+            return Command::SUCCESS;
+        }
+
+        return $this->updateMosparo($input, $output, $versionData, true);
+    }
+
+    protected function getTypeName($type, $subType = null): string
+    {
+        if ($type === 'general') {
+            if ($subType === 'minPhpVersion') {
+                return 'Minimum PHP version';
+            }
+
+            return 'General';
+        } else if ($type === 'phpExtension') {
+            if ($subType !== null) {
+                return $subType;
+            }
+
+            return 'PHP Extension';
+        } else if ($type === 'writeAccess') {
+            if ($subType !== null) {
+                return $subType;
+            }
+
+            return 'Write access';
+        }
+
+        return $type;
     }
 }
