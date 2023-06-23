692bfad495e3ac5052c842f276b56e2bf60248d3
Matthias Zobrist <matthias.zobrist@zepi.net>
2023-06-23 06:54:18 +0200

    Renamed the `validSubmission` to `lastSubmission` The initial name, `submission`, was misleading. But the new name `validSubmission` is also misleading because the property contains the last submission and not only the last valid one.
    With this commit, we renamed the property to `lastSubmission`.

--- src/Entity/Submission.php
+++ src/Entity/Submission.php
@@ -112,13 +112,13 @@ class Submission implements ProjectRelatedEntityInterface
     public function setSubmitToken(?SubmitToken $submitToken): self
     {
         // unset the owning side of the relation if necessary
-        if ($submitToken === null && $this->submitToken !== null && $this->submitToken->getValidSubmission() === $this) {
-            $this->submitToken->setValidSubmission(null);
+        if ($submitToken === null && $this->submitToken !== null && $this->submitToken->getLastSubmission() === $this) {
+            $this->submitToken->setLastSubmission(null);
         }
 
         // set the owning side of the relation if necessary
-        if ($submitToken !== null && $submitToken->getValidSubmission() !== $this) {
-            $submitToken->setValidSubmission($this);
+        if ($submitToken !== null && $submitToken->getLastSubmission() !== $this) {
+            $submitToken->setLastSubmission($this);
         }
 
         $this->submitToken = $submitToken;
