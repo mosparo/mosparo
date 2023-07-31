268586700a1a66fe136579401df10af2474eed5a
Matthias Zobrist <matthias.zobrist@zepi.net>
2023-07-31 19:29:08 +0200

    Fixed the performance issue with the statistic data (#121)

--- src/Controller/Api/V1/Statistic/StatisticApiController.php
+++ src/Controller/Api/V1/Statistic/StatisticApiController.php
@@ -4,11 +4,9 @@ namespace Mosparo\Controller\Api\V1\Statistic;
 
 use DateInterval;
 use DateTime;
-use DateTimeInterface;
-use Doctrine\ORM\EntityManagerInterface;
 use Exception;
-use Mosparo\Entity\Submission;
 use Mosparo\Helper\ProjectHelper;
+use Mosparo\Helper\StatisticHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -30,7 +28,7 @@ class StatisticApiController extends AbstractController
     /**
      * @Route("/by-date", name="statistic_api_by_date")
      */
-    public function byDate(Request $request, EntityManagerInterface $entityManager): Response
+    public function byDate(Request $request, StatisticHelper $statisticHelper): Response
     {
         // If there is no active project, we cannot do anything.
         if (!$this->projectHelper->hasActiveProject()) {
@@ -38,7 +36,7 @@ class StatisticApiController extends AbstractController
         }
 
         // Get the time range (in seconds) and calculate the start date
-        $startDate = false;
+        $startDate = null;
         if ($request->query->has('range')) {
             $range = intval($request->query->get('range'));
 
@@ -56,33 +54,7 @@ class StatisticApiController extends AbstractController
         }
 
         // Get the submissions
-        $queryBuilder = $entityManager->createQueryBuilder();
-        $queryBuilder
-            ->select('e')
-            ->from(Submission::class, 'e')
-            ->where('(e.spam = 1 OR e.valid IS NOT NULL)');
-
-        if ($startDate) {
-            $queryBuilder
-                ->andWhere('e.submittedAt > :startDate')
-                ->setParameter(':startDate', $startDate->format(DateTimeInterface::ATOM));
-        }
-
-        $submissions = $queryBuilder->getQuery()->getResult();
-
-        // Collect the statistic data
-        $data = ['numberOfValidSubmissions' => 0, 'numberOfSpamSubmissions' => 0, 'numbersByDate' => []];
-        foreach ($submissions as $submission) {
-            $type = ($submission->isSpam() || !$submission->isValid()) ? 'numberOfSpamSubmissions' : 'numberOfValidSubmissions';
-            $data[$type]++;
-
-            $day = $submission->getSubmittedAt()->format('Y-m-d');
-            if (!isset($data['numbersByDate'][$day])) {
-                $data['numbersByDate'][$day] = ['numberOfValidSubmissions' => 0, 'numberOfSpamSubmissions' => 0];
-            }
-
-            $data['numbersByDate'][$day][$type]++;
-        }
+        $data = $statisticHelper->getStatisticData($startDate);
 
         return new JsonResponse(['result' => true, 'data' => $data]);
     }
