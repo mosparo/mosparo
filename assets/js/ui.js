4dce6bc3ba6650994b582962e4022a48101bb5e4
Matthias Zobrist <matthias.zobrist@zepi.net>
2024-11-15 19:57:07 +0100

    Fixed the project dropdown style and functionality

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,14 +1,14 @@
 let searchTimeout;
 
 let setFocusOnFirstVisibleProject = function () {
-    $('.dropdown-projects-list .project-item.focus').removeClass('focus');
-    $('.dropdown-projects-list .project-item:visible').first().addClass('focus');
+    $('.dropdown-projects-list .project-group-node.focus').removeClass('focus');
+    $('.dropdown-projects-list .project-group-node:visible').first().addClass('focus');
 };
 
 let searchProject = function () {
     let query = $('.project-dropdown-menu input').val().toLowerCase();
 
-    $('.project-dropdown-menu .project-group-node').each(function () {
+    $('.project-dropdown-menu .project-group-node:not(.disabled)').each(function () {
         let name = $(this).children('.project-group-node-header').children('.project-group-node-label').text().toLowerCase();
 
         if (name.indexOf(query) !== -1) {
@@ -18,15 +18,15 @@ let searchProject = function () {
         }
     });
 
-    let visibleNodes = $('.project-dropdown-menu .project-group-node:not(.d-none)');
+    let visibleNodes = $('.project-dropdown-menu .project-group-node:not(.d-none):not(.disabled)');
     visibleNodes.find('.project-group-node').removeClass('d-none');
     visibleNodes.parents().removeClass('d-none');
 
-    if ($('.project-dropdown-menu .project-item:visible').length) {
+    if ($('.project-dropdown-menu .project-group-node:visible:not(.disabled)').length) {
         setFocusOnFirstVisibleProject();
-        $('.dropdown-item.no-projects-found').addClass('d-none');
+        $('.dropdown-projects-list .no-search-results-found').addClass('d-none');
     } else {
-        $('.dropdown-item.no-projects-found').removeClass('d-none');
+        $('.dropdown-projects-list .no-search-results-found').removeClass('d-none');
     }
 
     searchTimeout = null;
@@ -47,7 +47,7 @@ $(document).ready(function () {
             }
 
             setFocusOnFirstVisibleProject();
-            $('.dropdown-projects-list .project-item.focus')[0].scrollIntoView({ block: 'nearest' });
+            $('.dropdown-projects-list .project-group-node.focus')[0].scrollIntoView({ block: 'nearest' });
         }
     });
 
@@ -59,25 +59,65 @@ $(document).ready(function () {
         }
     });
 
+    function getPreviousVisibleNode(el)
+    {
+        let prevEl = el.prevAll(':not(.focus):not(.disabled):visible:first');
+        if (!prevEl.length && !el.parent().hasClass('project-group-tree')) {
+            prevEl = el.parent().parent();
+        }
+
+        let childrenEl = prevEl.children('.project-group-node-children');
+        if (prevEl.get(0) !== el.parent().parent().get(0) && prevEl.hasClass('open') && childrenEl.length && childrenEl.children(':visible:not(.disabled)').length) {
+            prevEl = findLast(childrenEl.children(':visible:not(.disabled):last'));
+        }
+
+        return prevEl;
+    }
+
+    function findLast(el)
+    {
+        let childrenEl = el.children('.project-group-node-children');
+        if (el.hasClass('open') && childrenEl.length && childrenEl.children(':visible:not(.disabled)').length) {
+            el = findLast(childrenEl.children(':visible:not(.disabled):last'));
+        }
+
+        return el;
+    }
+
+    function getNextVisibleNode(el, noRecursive)
+    {
+        let nextEl = el.nextAll(':visible:not(.disabled):first');
+        let childrenEl = el.children('.project-group-node-children');
+        if (!noRecursive && el.hasClass('open') && childrenEl.length && childrenEl.children(':visible:not(.disabled)').length) {
+            nextEl = childrenEl.children(':visible:not(.disabled):first');
+        }
+
+        if (!nextEl.length && !el.parent().hasClass('project-group-tree')) {
+            nextEl = getNextVisibleNode(el.parent().parent(), true);
+        }
+
+        return nextEl;
+    }
+
     inputEl.on('keydown', function (ev) {
         if (ev.keyCode === 38 || ev.keyCode === 40 || ev.keyCode === 13) {
-            let focusedProject = $('.dropdown-projects-list .project-item.focus');
+            let focusedNode = $('.dropdown-projects-list .project-group-node.focus');
             if (ev.keyCode === 38) {
-                let prevProject = focusedProject.prevAll(":visible:first");
-                if (prevProject.length) {
-                    focusedProject.removeClass('focus');
-                    prevProject.addClass('focus');
-                    prevProject[0].scrollIntoView({ block: 'nearest' });
+                let prevNode = getPreviousVisibleNode(focusedNode);
+                if (prevNode.length) {
+                    focusedNode.removeClass('focus');
+                    prevNode.addClass('focus');
+                    prevNode[0].scrollIntoView({ block: 'nearest' });
                 }
             } else if (ev.keyCode === 40) {
-                let nextProject = focusedProject.nextAll(':visible:first');
-                if (nextProject.length) {
-                    focusedProject.removeClass('focus');
-                    nextProject.addClass('focus');
-                    nextProject[0].scrollIntoView({ block: 'nearest' });
+                let nextNode = getNextVisibleNode(focusedNode);
+                if (nextNode.length) {
+                    focusedNode.removeClass('focus');
+                    nextNode.addClass('focus');
+                    nextNode[0].scrollIntoView({ block: 'nearest' });
                 }
             } else if (ev.keyCode === 13) {
-                window.location.href = focusedProject.attr('href');
+                focusedNode.children('.project-group-node-header').find('.project-group-node-label').click();
             }
 
             ev.stopPropagation();
