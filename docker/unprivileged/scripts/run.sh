7a2bd4974ddae952ce0119b72b42ec29a70c43bb
Matthias Zobrist <matthias.zobrist@zepi.net>
2025-02-24 19:10:38 +0100

    Fixed the PHP process in the unprivileged image

--- docker/unprivileged/scripts/run.sh
+++ docker/unprivileged/scripts/run.sh
@@ -8,7 +8,11 @@ rm -rf /mosparo/var/cache/prod
 [ -d /mosparo/var/data ] || mkdir /mosparo/var/data
 
 if [ $MOSPARO_RUN_PHP_FPM -eq 1 ]; then
-  php-fpm -D -R
+  if [ $MOSPARO_RUN_NGINX -eq 1 ]; then
+    php-fpm -D
+  else
+    php-fpm -F
+  fi
 fi
 
 if [ $MOSPARO_RUN_NGINX -eq 1 ]; then
