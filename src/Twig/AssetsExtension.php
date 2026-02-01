95566bcc9c0d540a456eb96623d01a4541ca5349
Matthias Zobrist <matthias.zobrist@zepi.net>
2026-02-01 17:24:49 +0100

    Replaced the illustrations because of the changed license

--- src/Twig/AssetsExtension.php
+++ src/Twig/AssetsExtension.php
@@ -2,23 +2,35 @@
 
 namespace Mosparo\Twig;
 
+use Mosparo\Helper\InterfaceHelper;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\TwigFunction;
 
 class AssetsExtension extends AbstractExtension
 {
     protected RequestStack $requestStack;
 
-    public function __construct(RequestStack $requestStack)
+    protected InterfaceHelper $interfaceHelper;
+
+    public function __construct(RequestStack $requestStack, InterfaceHelper $interfaceHelper)
     {
         $this->requestStack = $requestStack;
+        $this->interfaceHelper = $interfaceHelper;
     }
 
     public function getFilters(): array
     {
         return [
             new TwigFilter('add_path_prefix', [$this, 'addPathPrefix'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('get_illustration_path', [$this, 'getIllustrationPath']),
         ];
     }
 
@@ -33,5 +45,13 @@ class AssetsExtension extends AbstractExtension
         }
 
         return $html;
+    }
+
+    public function getIllustrationPath(string $illustrationName): string
+    {
+        $colorPaths = ['light', 'dark'];
+        $colorMode = $this->interfaceHelper->determineColorMode($this->requestStack->getMainRequest());
+        $colorPath = in_array($colorMode, $colorPaths) ? $colorMode : 'light';
+        return sprintf('%s/%s/%s', 'build/images/icons', $colorPath, $illustrationName);
     }
 }
